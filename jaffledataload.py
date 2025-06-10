9247433d21d79d60add6c17d81af6132eb3d55c4
vaishnavip3333 <vaishnaviipandeyy3333@gmail.com>
2025-06-10 18:10:25 +0530

    first day commit

---
+++ jaffledataload.py
@@ -0,0 +1,54 @@
+import os
+import pandas as pd
+from sqlalchemy import create_engine
+
+
+user = 'root'
+password = 'root'
+host = 'localhost'
+port = '3306'
+database = 'jaffle_shop'
+
+
+csv_folder = '/home/vaishnavi/csv_files'
+
+
+engine = create_engine(f'mysql+mysqlconnector://{user}:{password}@{host}:{port}/{database}')
+
+def get_table_name(filename):
+    if filename.startswith('raw_'):
+        return filename[4:].split('.')[0]
+    return filename.split('.')[0]
+
+def load_csv_to_mysql(csv_path, table_name, chunk_size=1000):
+    print(f"Loading {csv_path} into table '{table_name}'")
+    df = pd.read_csv(csv_path)
+    total_rows = len(df)
+    print(f"Total rows to load: {total_rows}")
+
+    for start in range(0, total_rows, chunk_size):
+        end = min(start + chunk_size, total_rows)
+        df_chunk = df.iloc[start:end]
+       
+        if start == 0:
+            if_exists_option = 'replace'
+        else:
+            if_exists_option = 'append'
+        df_chunk.to_sql(name=table_name, con=engine, if_exists=if_exists_option, index=False)
+        print(f"Loaded rows {start} to {end} successfully.")
+
+def main():
+   
+    files = [f for f in os.listdir(csv_folder) if f.endswith('.csv') and f.startswith('raw_')]
+
+    if not files:
+        print(f"No CSV files found in {csv_folder} starting with 'raw_'.")
+        return
+
+    for file in files:
+        csv_path = os.path.join(csv_folder, file)
+        table_name = get_table_name(file)
+        load_csv_to_mysql(csv_path, table_name)
+
+if __name__ == "__main__":
+    main()
